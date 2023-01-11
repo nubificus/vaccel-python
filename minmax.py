397a8e05b7ffd9bc176e10dda72151dac989d0e6
marrgko <94738375+marrgko@users.noreply.github.com>
2023-01-11 11:10:21 +0200

    Add docstrings

---
+++ minmax.py
@@ -0,0 +1,77 @@
+from vaccel.session import Session
+from typing import List
+from vaccel.genop import Genop, VaccelArg, VaccelOpType
+import struct
+import copy
+
+class MinMax:
+
+    """A MinMax model vAccel resource."""
+
+    def_arg_write: float = bytes(100 * " ", encoding="utf-8")
+
+    __op__ = VaccelOpType.VACCEL_MINMAX
+
+    @staticmethod
+    def __genop__(arg_read: List[VaccelArg], arg_write: List[VaccelArg], index: int) -> str:
+        """
+        Performs the genop operation provided in arg_read.
+
+        Returns the content of the arg_write indicated by index.
+        """
+        ses = Session(flags=0)
+        Genop.genop(ses, arg_read, arg_write)
+        return struct.unpack('d', arg_write[index].raw_content[:8])[0]
+
+    @staticmethod
+    def __parse_ndata__(ndata: "str | bytes") -> bytes:
+        """
+        Reads data from file.
+        Returns file content.
+
+        Parameters
+        ----------
+        ndata : `str` or `bytes`
+            Filename or bytes of the object.
+
+        Returns
+        ----------
+        ndata : `bytes`
+        """
+        if not isinstance(ndata, str) and not isinstance(ndata, bytes):
+            raise TypeError(
+                f"Invalid ndata type. Expected str or bytes, got {type(ndata)}.")
+
+        if isinstance(ndata, str):
+            with open(ndata, "rb") as ndatafile:
+                ndata = ndatafile.read()
+
+        return ndata
+
+    @classmethod
+    def minmax(self, indata: int, ndata: "str | bytes", low_threshold: "int", high_threshold: "int"):
+        """
+        Performs the MinMax operation using vAccel over genop.
+
+        Parameters
+        ----------
+        indata : `int`
+        ndata : `str | bytes`
+        low_theshold : `int` 
+        high_threshold : `int`
+
+        Returns
+        ----------
+        outdata : `float`
+        min : `float`
+        max : `float` 
+        """
+        ndata = self.__parse_ndata__(ndata=ndata)
+        arg_read = [VaccelArg(data=int(self.__op__)),
+                    VaccelArg(data=ndata),
+                    VaccelArg(data=indata),
+                    VaccelArg(data=low_threshold),
+                    VaccelArg(data=high_threshold)]
+        
+        arg_write = [VaccelArg(data=self.def_arg_write), VaccelArg(data=copy.deepcopy(self.def_arg_write)), VaccelArg(data=copy.deepcopy(self.def_arg_write))]
+        return self.__genop__(arg_read=arg_read, arg_write=arg_write, index=1)
---
+++ pynq_parallel.py
@@ -0,0 +1,44 @@
+from vaccel.session import Session
+from typing import List
+from vaccel.genop import Genop, VaccelArg, VaccelOpType
+import struct
+
+class Pynq_parallel:
+
+    __op__ = VaccelOpType.VACCEL_PYNQ_PARALLEL
+    
+    def_arg_write: bytes = bytes(100 * " ", encoding="utf-8")
+
+    @staticmethod
+    def __genop__(arg_read: List[VaccelArg], arg_write: List[VaccelArg], index: int) -> int:
+        """
+        Performs the genop operation provided in arg_read.
+
+        Returns the content of the arg_write indicated by index.
+        """
+        ses = Session(flags=0)
+        Genop.genop(ses, arg_read, arg_write)
+        return struct.unpack('d', arg_write[index].raw_content[:8])[0]
+
+    @classmethod
+    def pynq_parellel(self, a:float,len_a:int):
+        
+        """
+        Pynq parallel using vAccel over genop.
+
+        Parameters
+        ----------
+        a : `float`
+        len_a : `int`
+
+        Returns
+        ----------
+        b : `float`
+        add_out : `float`
+        mult_out : `float`
+        """
+        
+        arg_read = [VaccelArg(data=int(self.__op__)),
+                    VaccelArg(data=a),VaccelArg(data=len_a)]
+        arg_write = [VaccelArg(data=self.def_arg_write),VaccelArg(data=self.def_arg_write)]
+        return self.__genop__(arg_read=arg_read, arg_write=arg_write, index=0)
