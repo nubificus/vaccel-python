88ad8eebf174f1958104700c4978f3a6da40ed80
Maria Goutha <mgouth@nubificus.co.uk>
2023-03-21 19:07:13 +0200

    Add exec

--- vaccel/exec.py
+++ vaccel/exec.py
@@ -2,6 +2,19 @@ from vaccel.session import Session
 from typing import List
 from vaccel.genop import Genop, VaccelArg, VaccelOpType
 from vaccel._vaccel import lib, ffi
+from vaccel.error import VaccelError
+import sys 
+from ctypes import *
+
+class Object:
+    def obj(self, flags):
+        """Create a new vAccel object"""
+
+        self._inner = ffi.new("struct vaccel_shared_object *")
+        ret = lib.vaccel_obj_init(self._inner, flags)
+        if ret != 0:
+            raise VaccelError(ret, "Could not initialize session")
+
 
 class Exec_Operation:
     """An Image Operation model vAccel resource
@@ -27,6 +40,25 @@ class Exec_Operation:
         ses = Session(flags=0)
         Genop.genop(ses, arg_read, arg_write)
         return arg_write[index].content
+
+    @staticmethod
+    
+    def read_file(file_path):
+        with open(file_path, 'r') as f:
+            content = f.read()
+        return content
+    
+    def __parse_object__(obj: "str") -> bytes:
+        
+        if not isinstance(obj, str):
+            raise TypeError(
+                f"Invalid image type. Expected str or bytes, got {type(obj)}.")
+ 
+        if isinstance(obj, str):
+            with open(obj, "rb") as objfile:
+                obj = objfile.read()
+
+        return obj
 
 class Exec(Exec_Operation):
     """An Exec model vAccel resource.
@@ -56,7 +88,7 @@ class Exec(Exec_Operation):
         return self.__genop__(arg_read=arg_read_local, arg_write=arg_write, index=0)
 
 
-class Exec_with_resource(Exec_Operation):
+class Exec_with_resource(Exec_Operation,Object):
     """An Exec with resource model vAccel resource.
 
     Attributes:
@@ -66,7 +98,7 @@ class Exec_with_resource(Exec_Operation):
     __op__ = VaccelOpType.VACCEL_EXEC_WITH_RESOURCE
 
     @classmethod
-    def exec_with_resource(self, object: str, symbol: str, arg_read: List[VaccelArg], arg_write: List[VaccelArg]) -> str:
+    def exec_with_resource(self, obj: str, symbol: str, arg_read: List[VaccelArg], arg_write: List[VaccelArg]) -> str:
         """Performs the Exec using vAccel over genop.
 
         Args:
@@ -77,11 +109,21 @@ class Exec_with_resource(Exec_Operation):
         Returns:
             arg_write: A list of outputs
         """ 
-
+        #my_object=sys.path.append ('/usr/local/lib/libvaccel-noop.so')
         #my_object = ffi.cast("struct vaccel_shared_object *",object)
+        #path = "/usr/local/lib/libvaccel-noop.so"
+        #my_object = ctypes.CDLL("{}".format(path).encode())
+
+        #libc=cdll.LoadLibrary(obj)
+        # #libc = CDLL(obj)
+        # obj=bytes(100 * " ", encoding="utf-8")
+        # obj = self.read_file(obj)
+
+        #libc=cdll.LoadLibrary(obj)
+        #libc = CDLL(obj)
 
         arg_read_local = [VaccelArg(data=int(self.__op__)),
-                    VaccelArg(data=object),VaccelArg(data=symbol)] + arg_read
+                          VaccelArg(data=obj),VaccelArg(data=symbol)] + arg_read
         arg_write = [VaccelArg(data=bytes(100 * " ", encoding="utf-8"))]
         return self.__genop__(arg_read=arg_read_local, arg_write=arg_write, index=0)
 
