31549fa97006ae4066fbee59db1d3c6c14222e72
Anastassios Nanos <ananos@nubificus.co.uk>
2023-06-24 07:52:43 +0000

    update to un-break tests
    Signed-off-by: Anastassios Nanos <ananos@nubificus.co.uk>

--- tests/test_exec.py
+++ tests/test_exec.py
@@ -1,5 +1,4 @@
 from vaccel.exec import Exec, Exec_with_resource
-from vaccel.exec_many import Exec_with_many_resources
 from vaccel.genop import VaccelArg
 
 lib = "/usr/local/lib/libmytestlib.so"
@@ -7,7 +6,10 @@ object = "/usr/local/lib/libmytestlib.so"
 sym = "mytestfunc"
 arg_read = [1048576,12323,134366,1212]
 
-arg_write = ['                      ']
+#arg_write = ['                       ']
+mybytes = bytes((len("I got this input: ") + len("1048576")) * " ", encoding='utf-8')
+
+arg_write = [mybytes]
 
 
 # def test_exec_genop():
@@ -19,5 +21,5 @@ arg_write = ['                      ']
 def test_exec_with_res():
     res = Exec_with_resource.exec_with_resource(object, sym, arg_read, arg_write)
     #assert res == ("")
-    #assert res == ("I got this input: %d\n" % arg_read)
+    assert str(arg_write[0].decode('utf-8').strip()) == ("I got this input: %d" % arg_read[0])
 
