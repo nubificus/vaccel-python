bced7e97e310b0309e1a7cc5c0fb6d5ef910424c
Kostis Papazafeiropoulos <papazof@gmail.com>
2025-04-13 04:07:59 +0000

    ci: Hotfix workflows for latest vaccel/runners
    Hotfix workflows to use the current runner tag and vaccel installation
    method
    
    Signed-off-by: Kostis Papazafeiropoulos <papazof@gmail.com>

--- _docs/autogen.sh
+++ _docs/autogen.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
+
 source /venv/.env/bin/activate
-wget https://s3.nbfc.io/nbfc-assets/github/vaccelrt/master/x86_64/Release-deb/vaccel-0.5.0-Linux.deb
-dpkg -i vaccel-0.5.0-Linux.deb
-rm -fr vaccel-0.5.0-Linux.deb
+
 python builder.py
-export VACCEL_BACKENDS=/usr/local/lib/libvaccel-noop.so 
-export LD_LIBRARY_PATH=/usr/local/lib 
+export VACCEL_PLUGINS=libvaccel-noop.so 
 export PYTHONPATH=$PYTHONPATH:.
 python setup.py install
 rm -fr ./dist ./build ./.eggs ./vaccel_python.egg*
+
 lazydocs vaccel --overview-file index --src-base-url https://github.com/nubificus/python-vaccel --output-path ./_docs/docs
