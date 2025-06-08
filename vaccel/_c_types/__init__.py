4faeb720a9d460a44c866b01d77f794b7cd9f4f7
Kostis Papazafeiropoulos <papazof@gmail.com>
2025-06-08 18:34:38 +0000

    feat: Add C type wrapper and tensor methods for NumPy array conversions
    Add helpers for NumPy array conversions
    - Add `CType`-based wrapper for NumPy arrays
    - Add methods to create tensors from NumPy arrays
    - Add methods to create NumPy arrays from tensors
    - Split `TensorType` convertors into separate classes
    
    PR: https://github.com/nubificus/vaccel-python/pull/24
    Signed-off-by: Kostis Papazafeiropoulos <papazof@gmail.com>
    Reviewed-by: Anastassios Nanos <ananos@nubificus.co.uk>
    Approved-by: Anastassios Nanos <ananos@nubificus.co.uk>

--- vaccel/_c_types/__init__.py
+++ vaccel/_c_types/__init__.py
@@ -1,10 +1,22 @@
 # SPDX-License-Identifier: Apache-2.0
 
+"""Common interfaces for C types and Python type wrappers."""
+
 from .types import CAny, CType
 from .wrappers.cbytes import CBytes
 from .wrappers.cfloat import CFloat
 from .wrappers.cint import CInt
 from .wrappers.clist import CList
+from .wrappers.cnumpyarray import CNumpyArray
 from .wrappers.cstr import CStr
 
-__all__ = ["CAny", "CBytes", "CFloat", "CInt", "CList", "CStr", "CType"]
+__all__ = [
+    "CAny",
+    "CBytes",
+    "CFloat",
+    "CInt",
+    "CList",
+    "CNumpyArray",
+    "CStr",
+    "CType",
+]
