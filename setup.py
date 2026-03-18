ed31e68e6cec6c4d7be26759a69d3045a6e31bba
Kostis Papazafeiropoulos <papazof@gmail.com>
2026-03-18 00:06:34 +0000

    build: Update with new component versioning
    Update build and CI with new component versioning scheme:
    - Switch to custom versioning in `pyproject.toml`/`setup.py` to strip
      newly added version/tag metadata from wheels and generate
      PyPI-compatible versions
    - Add explicit dependency to core vAccel C package in wheel metadata
    - Amend `dispatch-prepare-release` workflow to reflect build versioning
      changes
    - Remove local `upload-to-s3` action and override the newly introduced
      upstream `generate-latest-artifacts` action with the package
      versioning scheme
    
    Signed-off-by: Kostis Papazafeiropoulos <papazof@gmail.com>

--- setup.py
+++ setup.py
@@ -5,8 +5,35 @@
 This module is only necessary for building the CFFI module library.
 """
 
+from typing import Any
+
+from packaging.version import Version
 from setuptools import setup
+from setuptools_scm.git import parse as git_parse
+from setuptools_scm.version import ScmVersion
+
+
+def custom_parse(root: str, config: Any) -> ScmVersion | None:
+    """Custom parse function that removes tag local segment.
+
+    Removes semver metadata from tags to generate pypi compatible versions.
+    """
+    version: ScmVersion | None = git_parse(root, config)
+    if version is None:
+        return None
+
+    # Strip local segment from parsed tag so setuptools-scm does not
+    # re-append it
+    base = str(version.tag).split("+")[0]
+    version.tag = Version(base)
+
+    return version
+
 
 setup(
     cffi_modules=["build_ffi.py:compile_ffi"],
+    use_scm_version={
+        "parse": custom_parse,
+        "version_scheme": "no-guess-dev",
+    },
 )
